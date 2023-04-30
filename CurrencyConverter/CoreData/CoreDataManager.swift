064cefd1213fd6096d8ec805cc1af7afd19e4f1e
yuraSud <sabadin85@icloud.com>
2023-04-30 10:16:56 +0300

    create Core Data

---
+++ CurrencyConverter/CoreData/CoreDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataManager.swift
+//  CurrencyConverter
+//
+//  Created by YURA																			 on 29.04.2023.
+//
+
+import Foundation
---
+++ CurrencyConverter/Extension/Date+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Date+Extension.swift
+//  CurrencyConverter
+//
+//  Created by YURA																			 on 29.04.2023.
+//
+
+import Foundation
